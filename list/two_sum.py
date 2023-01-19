703961910138d0f73c264da61a3dd2a1a5aab487
jungHyeonS <fbtls25@naver.com>
2023-01-19 22:24:54 +0900

    feat : 리스트 투포인터 공부 진행중

--- list/two_sum.py
+++ list/two_sum.py
@@ -1,17 +1,37 @@
 class Solution:
   def twoSum(self, nums: list[int], target: int) -> list[int]:
+    start = 0
+    end = len(nums) - 1
     res = []
-    for i in range(0,len(nums)-1):
-      for j in range(i+1,len(nums)):
-        if nums[i] + nums[j] == target:
-          res.append(i)
-          res.append(j)
+    while(True):
+      sum = nums[start] + nums[end]
+      print(sum)
+      if sum == target:
+        res.append(start)
+        res.append(end)
+        break
+
+      if sum > target:
+        end = end - 1
       
+      if sum < target:
+        start = start + 1
+
+    res.sort()
     return res
+
+    # res = []
+    # for i in range(0,len(nums)-1):
+    #   for j in range(i+1,len(nums)):
+    #     if nums[i] + nums[j] == target:
+    #       res.append(i)
+    #       res.append(j)
+      
+    # return res
 
     
 
 
 if __name__ == '__main__':
   solution = Solution()
-  solution.twoSum([3,2,4],6)
+  solution.twoSum([-1,-2,-3,-4,-5],-8)
