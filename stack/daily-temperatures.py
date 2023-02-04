d01d1c6243aa698abe849f10fe6ec9a160f659ec
jungHyeonS <fbtls25@naver.com>
2023-02-04 22:38:47 +0900

    feat : 데일리 테플릿 풀리 완료

--- stack/daily-temperatures.py
+++ stack/daily-temperatures.py
@@ -1,52 +1,27 @@
 from typing import List
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-      # print(temperatures)
       stack = []
       answer = []
       for i,daily in enumerate(temperatures):
-        # print(daily)
+        answer.append(0)
         if len(stack) < 1:
           stack.append({'key':i,'value':daily})
         else:
-          # if i < 3:
           while(True):
-            if len(stack) < 1:
-              break
-              
-            if stack[-1]['value'] < daily:
-              answer.append(i - stack[-1]['key'])
-              stack.pop()
-              stack.append({'key':i,'value':daily})
+            if len(stack) >= 1:
+              if stack[-1]['value'] < daily:
+                answer[stack[-1]['key']] = i - stack[-1]['key']
+                stack.pop()
+              else:
+                stack.append({'key':i,'value':daily})
+                break
             else:
               stack.append({'key':i,'value':daily})
               break
-              # print(i - stack[-1]['key'])
-        #   # print(stack[-1])
-        #   while(True):
-        #     count = 0
-        #     if stack[-1] < daily:
-        #       count += 1
-        #       stack.pop()
-        #       stack.append(daily)
-        #       answer.append(count)
-        #       break
-        #     else:
-        #       stack.append(daily)
-        #       break
-            
-          # while(True):
-          #   if stack[-1] < daily:
-          #     stack.pop()
-          #     break
-          #   else:
-          #     stack.append(daily)
-          #     break
-            
-      print(stack)
-      print(answer)
-      pass
+      return answer
     
 
 solution = Solution()
-solution.dailyTemperatures([73,74,75,71,69,72,76,73])
+# solution.dailyTemperatures([73,74,75,71,69,72,76,73])
+print(solution.dailyTemperatures([30,60,90]))
